5ffe58b618d6712b46f1976ddc0d1ee944baa890
anjana2401 <89989820+anjana2401@users.noreply.github.com>
2022-09-08 21:38:04 -0700

    Added Assignment Code
    Made modification for Assignment1

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 export default {
-	fetch(request)
-		return new Response('Hello CodeClub!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+  fetch (request) {
+    console.log(request.url);
+    console.log(request.method);
+    if (request.method == "POST"){
+      let data = {
+       CoderName: 'Anjana Maheshwari',
+       CoderLevel: 'Beginner'
+      };
+      let jsonData = JSON.stringify(data,null,2);
+
+      return new Response (jsonData, {
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+    }
+    else if (request.method == "GET") {
+      return new Response ('This is Anjana attending Code Club', {
+        headers: {
+          'content-type': 'text/plain',
+        }
+      });
+    }
 	},
-};
+} 
